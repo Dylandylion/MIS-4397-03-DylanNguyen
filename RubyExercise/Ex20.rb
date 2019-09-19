21f924599b65ddf343d62cc63c1895d60c09b392
DylanN <dnguyen099@yahoo.com>
2019-09-19 05:25:53 +0000

    919191 Updating RubyExercises

--- RubyExercise/Ex20.rb
+++ RubyExercise/Ex20.rb
@@ -25,12 +25,12 @@ rewind(current_file)
 puts "Let's print three lines:"
 
 current_line = 1
-print_a_line(current_Line, current_file
+print_a_line(current_line, current_file)
 
 
-current_line = current_line+1
-print_a_line(current_Line, current_file
+current_line = current_line + 1
+print_a_line(current_line, current_file)
 
 
-current_line = current_line+1
-print_a_line(current_Line, current_file
+current_line = current_line + 1
+print_a_line(current_line, current_file)
