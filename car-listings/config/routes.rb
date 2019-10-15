8f2c885f67eec79037c80a3c6c74ee36ddd10158
DylanN <dnguyen099@yahoo.com>
2019-10-15 02:52:27 +0000

    Updating my repo

--- car-listings/config/routes.rb
+++ car-listings/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :salesmen
   resources :dealerships
   resources :cars
+  root 'saleswomen#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- cars-app/config/routes.rb
+++ cars-app/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :cars
   get 'welcome/home', to: 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
+  root 'page#home'
+  #get 'page/home', to: 'page#home'
+  get 'page/about', to: 'page#about'
   #get 'car', to:'car'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
